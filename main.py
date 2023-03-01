bcd7f7d8fe681cc27ce805dda8a789bee90d3423
roliroli76 <108983491+roliroli76@users.noreply.github.com>
2023-03-01 11:50:17 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
-a = b = c = 10
+#options = ['load data', 'export data', 'analyze & predict']
 
-print(a, b, c)
-print(id(a), id(b), id(c))
+def DisplayOptions(options):
+    for i in range (len(options)):
+        print("{} - {}".format(i+1,options[i]))
+    choice = input("Select option above or press enter to exit:")
+    return choice
 
-a = 20
+options = ['load data', 'export data', 'analyze & predict']
+choice = 'x'
 
-print(a, b, c)
-print(id(a), id(b), id(c))
-print('-----------------------------')
 
-a = b = c = [1, 2, 3]
 
-print(a, b, c)
-print(id(a), id(b), id(c))
 
-a.append(4)
+while choice:
+    choice = DisplayOptions(options)
+    if choice:
+        try:
+            choice_num = int(choice)
+            if choice_num > 0 and choice_num < len(options):
+                print("you have selected  {} - {}".format(choice_num+1, options[choice_num]))
+            else:
+              print("choose a value from a list or press enter")
+        except:
+            print("You must enter a number")
+    else:
+        print("end")
 
-print(a, b, c)
-print(id(a), id(b), id(c))
-print('-----------------------------')
 
-x = 10
-y = 10
-
-print(id(x), id(y))
-y = y + 1 - 1
-print(id(x), id(y))
-
-y = y + 1234567890123 - 1234567890123
-print(id(x), id(y))
-
-y = y + 1234567890 - 1234567890
-print(id(x), id(y))
-print('-----------------------------')
