0a9cac88ec77cf2d26b03adb843fdde074a2be87
roliroli76 <dominik.rolek@gmail.com>
2023-04-05 14:51:07 +0200

    Revert "Revert "Add files via upload""
    This reverts commit b6bef07097daa359909781cbe6cc6e32f2b09823.

---
+++ Funkcje i wartości domyślne argumentów.py
@@ -0,0 +1,9 @@
+def show_progress(how_many, character = '*' ):
+    print(character * how_many)
+
+show_progress(10)
+show_progress(15)
+show_progress(30)
+show_progress(10, '-')
+show_progress(15, '+')
+
