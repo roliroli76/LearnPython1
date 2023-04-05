2660c39347fcef016a9fed7b3324e94e2fca0403
roliroli76 <dominik.rolek@gmail.com>
2023-04-05 14:50:51 +0200

    Revert "Revert "Add files via upload""
    This reverts commit 2fc7e21120423116d15a42ef50822e7cb8dd632c.

---
+++ Zagnieżdżanie pętli i to samo w postaci jednolinijkowej.py
@@ -0,0 +1,21 @@
+
+ports = ['WAW', 'KRK', 'GDN', 'KTW', 'WMI', 'WRO', 'POZ', 'RZE', 'SZZ',
+         'LUZ', 'BZG', 'LCJ', 'SZY', 'IEG', 'RDO']
+ports2 = ['WAW', 'KRK', 'GDN', 'KTW', 'WMI', 'WRO', 'POZ', 'RZE', 'SZZ',
+         'LUZ', 'BZG', 'LCJ', 'SZY', 'IEG', 'RDO']
+
+routes = [(start, stop) for start in ports for stop in ports]
+print(routes)
+print(len(routes))
+
+print("XXXXXXXXXXXXXXXXXXXXXXXXXX")
+
+routes = [(start, stop) for start in ports for stop in ports if start != stop]
+print(routes)
+print(len(routes))
+
+print("XXXXXXXXXXXXXXXXXXXXXXXXXX")
+
+routes = [(start, stop) for start in ports for stop in ports if start < stop ]
+print(routes)
+print(len(routes))
